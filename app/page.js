3b7a8ede1de1dc6420490d9fd66d2bf503cb51c0
soonhengshau <92926923+soonhengshau@users.noreply.github.com>
2024-10-13 20:32:03 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -48,7 +48,7 @@ export default function Home() {
         <h2 className="text-xl text-center mb-4">Test Registrations</h2>
 
         {/* Wrapping the table inside a div with overflow for mobile responsiveness */}
-        <div className="overflow-x-auto w-full">
+        <div className="overflow-x-auto w-11/12 lg:w-1/2 mx-auto">
           <table className="table-auto border-collapse border border-gray-300 w-full min-w-max">
             <thead>
               <tr>
@@ -81,10 +81,10 @@ export default function Home() {
                   <td className="border border-gray-300 px-2 py-2 text-sm md:text-base">
                     {entry[2]}
                   </td>
-                  <td className="border border-gray-300 px-2 py-2 text-sm md:text-base">
+                  <td className="border border-gray-300 px-2 py-2 text-sm md:text-base text-center">
                     {entry[3]}
                   </td>
-                  <td className="border border-gray-300 px-2 py-2">
+                  <td className="border border-gray-300 px-2 py-2 text-center">
                     {state.some((arr) => entry[0] === arr.child1_name) ? (
                       <button className="mt-2 px-2 py-1 bg-gray-500 text-white text-sm rounded-md">
                         Booked
